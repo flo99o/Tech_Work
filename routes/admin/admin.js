3de7833033d5ca1a1caec3018d2616c4f863be65
Florence Alonzeau <florencealonzeau18@gmail.com>
2020-09-29 17:08:39 +0200

    admin routes created

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -2,6 +2,36 @@ const connection = require('../../config')
 const express = require('express')
 const router = express.Router()
 
+// Delete Admin
+router.delete('/userDelete/:id',(req, res )=>{
+  const id = req.params.id
+
+  connection.query('DELETE FROM Job.users WHERE id=?', id, (err)=>{
+      if(err){
+          res.status(500).status('This user have not been deleted')         
+          
+        }else{
+          res.status(200).send('The user have been deleted')
+          
+      }
+    
+   
+  })
+ 
+})
+
+router.delete('/compagnyDelete/:id', (req, res)=>{
+    const id = req.params.id
+    connection.query('DELETE FROM Job.compagny WHERE = id?',id,(req, results)=>{
+        if(err){
+            res.status(500).send('This compagny have not been deleted')
+        }else{
+
+            res.status(200).send('The compagny have been deleted')
+        }
+    })
+})
+
 
 
 module.exports = router
--- routes/users/users.js
+++ routes/users/users.js
@@ -2,4 +2,11 @@ const connection = require('../../config')
 const express = require('express')
 const router = express.Router()
 
+
+
+
+router.get('/user', function (req, res) {
+    res.send('Hello World!');
+  });
+  
 module.exports = router
