8e3618ea85dc4ac49aeb5b43f29a5b8f8af42614
Florence Alonzeau <florencealonzeau18@gmail.com>
2020-10-06 15:53:29 +0200

    routes

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const bodyParser = require ('body-parser')
 var cors = require('cors')
 const morgan = require('morgan')
 
+const { authRoutes } = require('./routes/index');
+
 const port = process.env.PORT || 5000
 
 app.use(morgan('dev'))
@@ -13,9 +15,21 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
 
-app.use('/', router.users)
-app.use('/', router.admin)
-app.use('/', router.compagny)
+
+app.set('view engine', 'ejs')
+app.get('/', (req, res)=>{
+    res.render('home') 
+})
+
+
+
+
+app.use('/user', router.users)
+app.use('/admin', router.admin)
+app.use('/compagny', router.compagny)
+
+
+
 app.listen(port, () => {
     console.log (`listening on port ${port}`)
 })
