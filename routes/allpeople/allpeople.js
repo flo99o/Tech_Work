d8781a389956b745affc401443dcedd7059b09da
Florence Alonzeau <florencealonzeau18@gmail.com>
2020-10-09 12:19:33 +0200

    dev clean out

--- routes/allpeople/allpeople.js
+++ routes/allpeople/allpeople.js
@@ -3,20 +3,6 @@ const express = require('express')
 const router = express.Router()
 
 
-// allpeople can select one offer 
-router.get('/allPeopleSelectOffer/:id', (req, res)=>{
-    const id = req.params.id
-    connection.query('SELECT * FROM Job.offers WHERE id= ?',id, (err, results)=>{
-      if(err){
-        res.status(500).send(err)
-      }else{
-          console.log(results)
-        res.status(200).send('this user select this offer.')
-      }
-    })
-  })
-
-
 
 
 module.exports = router
--- routes/users/users.js
+++ routes/users/users.js
@@ -6,38 +6,4 @@ const router = express.Router()
 
 
 
-// user can post his info (checked good)
-router.post('/userPost', (req, res) => {
-  const content = req.body
-  console.log(content)
-  connection.query(`INSERT INTO users (first_name, last_name,birth_date,resume,email, password) VALUES ("${content.first_name}","${content.last_name}","${content.birth_date}","${content.resume}","${content.email}","${content.passeword}")`, (err, results) => {
-    if (err) {
-      console.log(err)
-      res.status(500).send('Can\'t post this user')
-    } else {
-      console.log(err)
-      res.status(200).json(results)
-    }
-  })
-})
-
-// user can update his info
-router.put('/')
-
-
-// users can update their information + password
-router.put('/updateUserInfo/:id', (req, res) => {
-  const details = req.body
-  connection.query(`UPDATE users SET resume="${details.resume}" WHERE =id"${details.id}"`, (err, results) => {
-    if (err) {
-      console.log(err)
-      res.status(500).status('The user information have not been updated')
-    } else {
-      res.status(200).json(results)
-    }
-  })
-
-})
-
-
 module.exports = router
