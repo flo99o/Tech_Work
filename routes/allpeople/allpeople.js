e3543b538fbd25762edf81a650c9cb189bc95676
tracy zumbu garcia <tracyzumbu@lilo.org>
2020-10-18 11:18:09 +0200

    fixed register routes and deleteCompagny routes

--- routes/allpeople/allpeople.js
+++ routes/allpeople/allpeople.js
@@ -4,37 +4,59 @@ const bcrypt = require("bcryptjs");
 const router = express.Router();
 
 //Register (works)
-
+ 
 router.post("/register", (req, res) => {
   const content = req.body;
   const password = bcrypt.hashSync(content.password);
+  console.log(req.body);
+  //requête for compagny
+  if (content.compagny_name) {
+    console.log('compagny');
+    connection.query(
+      `INSERT INTO Job.users(first_name, last_name,password,email, logo, type, compagny_name, phone, description_compagny) VALUES ("${content.first_name}", "${content.last_name}", "${password}","${content.email}","${content.logo}", "${content.userType}", "${content.compagny_name}", "${content.phone}", "${content.description_compagny}")`,
+      (err, results) => {
+        if (err) {
+          console.log("err :", err);
+          res.status(500).send("The content have not been register");
+        } else {
+          console.log("user has been register");
+        }
+      }
+    );
+    console.log('compagny');
+    connection.query(
+
+      `INSERT INTO Job.compagnies SELECT users.userID, users.compagny_name FROM Job.users WHERE userID = LAST_INSERT_ID()`
+    );
+  } //requête for others users 
+  else {
+    console.log('user');
+    connection.query(
+      `INSERT INTO Job.users(first_name, last_name,password,email, logo, type, phone) VALUES ("${content.first_name}", "${content.last_name}", "${password}","${content.email}","${content.logo}", "${content.userType}", "${content.phone}")`,
+      (err, results) => {
+        if (err) {
+          console.log("err :", err);
+        } else {
+          console.log("user has been register");
+        }
+      }
+    );
+  }
   connection.query(
-    `INSERT INTO Job.users(first_name, last_name,password,email, logo, type, compagny_name, phone, description_compagny) VALUES ("${content.first_name}", "${content.last_name}", "${password}","${content.email}","${content.logo}", "${content.type}", "${content.compagny_name}", "${content.phone}", "${content.description_compagny}")`,
+    `SELECT * FROM Job.users WHERE email = "${content.email}"`,
     (err, results) => {
       if (err) {
         console.log("err :", err);
-        res.status(500).send("The content have not been register");
       } else {
-        console.log('user has been register');
-        
+        res.status(200).send({
+          userID: results[0].userID,
+          userType: results[0].type,
+          isLogged: true,
+          compagny_name: results[0].compagny_name
+        });
       }
     }
   );
-  connection.query(
-    `INSERT INTO Job.compagnies (compagny_name) VALUES ("${content.compagny_name}")`
-  );
-  connection.query(`SELECT * FROM Job.users WHERE email = "${content.email}"`,(err, results) => {
-    if (err) {
-      console.log("err :", err);
-    } else {
-      res.status(200).send({
-        userID: results[0].userID,
-        userType: results[0].type,
-        isLogged: true
-      });
-      console.log(results[0].userID)
-    }
-  })
 });
 
 //getOffers pour fichier allPeople (works)
@@ -42,7 +64,7 @@ router.get("/getoffers", (req, res) => {
   connection.query(
     `SELECT * FROM Job.offers
       INNER JOIN Job.compagnies
-      ON compagnies.compagnyID = offers.compagny_id
+      ON compagnies.compagny_name = offers.compagny_name
       INNER JOIN Job.users
       ON users.userID = offers.user_id
       `,
@@ -91,6 +113,7 @@ router.delete("/deleteUserAccount/:id", (req, res) => {
 // get detail's offer (works)
 router.get("/getOffer/:idJob", (req, res) => {
   const offerID = req.params.idJob;
+  console.log('offerID:', offerID)
   connection.query(
     `SELECT * FROM Job.offers
       INNER JOIN Job.compagnies
@@ -109,7 +132,7 @@ router.get("/getOffer/:idJob", (req, res) => {
   );
 });
 
-//all user get his details
+//all user get his details (works)
 router.get("/userDetails/:userID", (req, res) => {
   const userID = req.params.userID;
   connection.query(
@@ -128,17 +151,20 @@ router.get("/userDetails/:userID", (req, res) => {
 
 // all users can update his info (works)
 router.put("/updateProfile/:userID", (req, res) => {
+  console.log(req.body);
   const userID = req.params.userID;
   const newDetails = req.body;
+  const password = bcrypt.hashSync(newDetails.password)
   console.log(newDetails);
   connection.query(
     "UPDATE Job.users SET ? WHERE users.userID = ? ",
-    [newDetails, userID],
+    [{...newDetails,password} , userID],
     (err, results) => {
       if (err) {
         console.log(err);
       } else {
         res.status(200).send("Profile updated");
+        console.log("results: ", results);
       }
     }
   );
--- routes/compagny/compagny.js
+++ routes/compagny/compagny.js
@@ -8,7 +8,7 @@ router.get("/getMyOffers/:id", (req, res) => {
   connection.query(
     `SELECT * FROM Job.offers
       INNER JOIN Job.compagnies
-      ON compagnies.compagnyID = offers.compagny_id
+      ON compagnies.compagny_name = offers.compagny_name
       INNER JOIN Job.users
       ON users.userID = offers.user_id
           WHERE user_id = ${userID} `,
@@ -28,7 +28,7 @@ router.post("/createad", (req, res) => {
   const content = req.body;
   console.log(content);
   connection.query(
-    `INSERT INTO Job.offers (job_name, description_position, prerequisite, location, wage, contract, compagny_id, user_id) VALUES ("${content.job_name}", "${content.description_position}", "${content.prerequisite}", "${content.location}", "${content.wage}", "${content.contract}", "${content.compagnyID}", "${content.userID}")`,
+    `INSERT INTO Job.offers (job_name, description_position, prerequisite, location, wage, contract, compagny_id, user_id, compagny_name) VALUES ("${content.job_name}", "${content.description_position}", "${content.prerequisite}", "${content.location}", "${content.wage}", "${content.contract}", "${content.userID}", "${content.userID}", "${content.compagny_name}")`,
     (err, resultat) => {
       if (err) {
         console.log(err);
@@ -95,10 +95,12 @@ router.delete("/deleteCompagny/:compagny_name", (req, res) => {
   const compagny_name = req.params.compagny_name;
 
   connection.query(
-    `DELETE Job.compagnies, Job.users
+    `DELETE Job.compagnies, Job.users, Job.offers
     FROM Job.users
     INNER JOIN Job.compagnies
     ON users.compagny_name = compagnies.compagny_name
+    INNER JOIN Job.offers
+    ON users.userID = offers.user_id
     WHERE compagnies.compagny_name = "${compagny_name}"`,
     (err, results) => {
       if (err) {
@@ -110,5 +112,19 @@ router.delete("/deleteCompagny/:compagny_name", (req, res) => {
     }
   );
 });
+
+//update a offer
+router.put("/updatead/:offerID", (req,res) => {
+  const offerID = req.params.offerID
+  const newDetails = req.body
+
+  connection.query( "UPDATE Job.offers SET ? WHERE offers.offerID = ?", [newDetails, offerID], (err, results) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.status(200).send("Profile updated");
+    }
+  })
+})
 
 module.exports = router;
